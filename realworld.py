13c4ba014d9bf8b7204eed5f9f214707c16dc8c2
Abhi <abhidubal1@gmail.com>
2020-04-30 18:03:42 -0600

    Changed batch size

--- realworld.py
+++ realworld.py
@@ -51,7 +51,7 @@ if True:
     model.cuda()
     criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
     criterion.cuda()
-    BATCH_SIZE = 600  # Was 12000, but I only have 12 GB RAM on my single GPU.
+    BATCH_SIZE = 6000  # Was 12000, but I only have 12 GB RAM on my single GPU.
     train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0, repeat=False,
                             sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=True)
     valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0, repeat=False,
