trying to fix softmax issues

run_transformer.py
@@ -278,8 +278,7 @@ def test(args):
 
     w2_param = []
     for name, param in model.named_parameters():
-        if name.__contains__("src_embed") or name.__contains__("tgt_embed") or name.__contains__("feed_forward")\
-                or name.__contains__("generator"):
+        if name.__contains__("feed_forward"):
             w2_param.append(np.prod(param.size()))
 
     print("Num parameters in original fc layer", np.sum(w2_param))

transformer/functional.py
@@ -26,7 +26,7 @@ def subsequent_mask(size):
     return torch.from_numpy(mask) == 0
 
 
-def attention(query, key, value, mask=None, dropout=None, matmul=None, softmax=None):
+def attention(query, key, value, mask=None, dropout=None, matmul=None):
     """
     Compute 'Scaled Dot Product Attention'
     """
@@ -34,6 +34,7 @@ def attention(query, key, value, mask=None, dropout=None, matmul=None, softmax=N
     scores = matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is not None:
         scores = scores.masked_fill_(mask == 0, value=-1e9)
+    softmax = nn.Softmax(dim=-1)
     p_attn = softmax(scores)
     if dropout is not None:
         p_attn = dropout(p_attn)

transformer/multihead_attention.py
@@ -21,7 +21,6 @@ class MultiHeadAttention(nn.Module):
         self.attn = None
         self.matmul = Matmul()
         self.dropout = nn.Dropout(p=dropout)
-        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, query, key, value, mask=None):
         """
@@ -35,7 +34,7 @@ class MultiHeadAttention(nn.Module):
         query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for l, x in
                              zip(self.linears, (query, key, value))]
         # 2) Apply attention on all the projected vectors in batch.
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout, matmul=self.matmul, softmax=self.softmax)
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout, matmul=self.matmul)
         # 3) "Concat" using a view and apply a final linear.
         x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
         return self.linears[-1](x)