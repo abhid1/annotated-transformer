31c9c60409dadccfecaf651a7ed4bc64a5723994
Abhi <abhidubal1@gmail.com>
2020-08-05 22:22:30 -0600

    trying to fix cuda

--- run_transformer.py
+++ run_transformer.py
@@ -57,7 +57,7 @@ def run_epoch(data_iter, model, loss_compute, args, SRC=None, TGT=None, valid_it
         # IF PRUNING
         #if compression_scheduler:
          #   compression_scheduler.on_minibatch_begin(epoch, minibatch_id=i, minibatches_per_epoch=steps_per_epoch)
-        out = model.forward(batch.src, batch.trg, batch.src_mask, batch.trg_mask)
+        out = model.forward(batch.src.cuda(), batch.trg.cuda(), batch.src_mask.cuda(), batch.trg_mask.cuda())
 
         # IF PRUNING
         #loss = loss_compute(out, batch.trg_y, batch.ntokens, i, epoch, steps_per_epoch, compression_scheduler)
@@ -75,10 +75,10 @@ def run_epoch(data_iter, model, loss_compute, args, SRC=None, TGT=None, valid_it
 
         if i % args.valid_every == 1 and valid_iter is not None:
             model.eval()
-            run_validation_bleu_score(model.module, SRC, TGT, valid_iter)
+            run_validation_bleu_score(model, SRC, TGT, valid_iter)
 
         if is_valid:
-            run_validation_bleu_score(model.module, SRC, TGT, valid_iter)
+            run_validation_bleu_score(model, SRC, TGT, valid_iter)
 
         # IF PRUNING
         #if compression_scheduler:
@@ -404,20 +404,18 @@ def test(args):
     distiller.utils.assign_layer_fq_names(model)
     stats_file = './acts_quantization_stats.yaml'
 
-    model_par = nn.DataParallel(model, device_ids=devices)
-
     if not os.path.isfile(stats_file):
         def eval_for_stats(model):
             valid_iter = MyIterator(val, batch_size=args.batch_size, device=0, repeat=False,
                                     sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn,
                                     train=False,
                                     sort=False)
-            model_par.eval()
-            run_epoch((rebatch(pad_idx, b) for b in valid_iter), model_par,
+            model.eval()
+            run_epoch((rebatch(pad_idx, b) for b in valid_iter), model,
                              MultiGPULossCompute(model.generator, criterion, devices=devices, opt=None), args,
                              SRC, TGT, valid_iter, is_valid=True)
 
-        collect_quant_stats(distiller.utils.make_non_parallel_copy(model_par), eval_for_stats, save_dir='.')
+        collect_quant_stats(model, eval_for_stats, save_dir='.')
 
     overrides = distiller.utils.yaml_ordered_load(overrides_yaml)
     quantizer = PostTrainLinearQuantizer(deepcopy(model), mode="ASYMMETRIC_UNSIGNED", overrides=overrides)
