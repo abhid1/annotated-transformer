869fb520b5145e7aa09c0a34b885fd14413c310b
Abhi <abhidubal1@gmail.com>
2020-08-07 20:51:45 -0600

    reverted layer norm

--- transformer/layer_norm.py
+++ transformer/layer_norm.py
@@ -3,17 +3,17 @@
 import torch
 
 import torch.nn as nn
-from distiller.modules import *
+# from distiller.modules import *
 
 
-class Std(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(Std, self).__init__()
-        self.args = args
-        self.kwargs = kwargs
-
-    def forward(self, x: torch.Tensor):
-        return torch.std(x, *self.args, **self.kwargs)
+# class Std(nn.Module):
+#     def __init__(self, *args, **kwargs):
+#         super(Std, self).__init__()
+#         self.args = args
+#         self.kwargs = kwargs
+#
+#     def forward(self, x: torch.Tensor):
+#         return torch.std(x, *self.args, **self.kwargs)
 
 
 class LayerNorm(nn.Module):
@@ -26,17 +26,8 @@ class LayerNorm(nn.Module):
         self.a_2 = nn.Parameter(torch.ones(features))
         self.b_2 = nn.Parameter(torch.zeros(features))
         self.eps = eps
-        self.Mean = Mean(dim=-1, keepdim=True)
-        self.Std = Std(dim=-1, keepdim=True)
-        self.eltwisemul1 = EltwiseMult()
-        self.eltwisesub1 = EltwiseSub()
-        self.eltwisediv = EltwiseDiv()
-        self.eltwiseadd = EltwiseAdd()
 
     def forward(self, x):
-        # mean = x.mean(dim=-1, keepdim=True)
-        mean = self.Mean(x)
-        # std = x.std(dim=-1, keepdim=True)
-        std = self.Std(x)
-        return self.eltwisediv(self.eltwisemul1(self.a_2, (self.eltwisesub1(x - mean))),
-                               self.eltwiseadd((std + self.eps), self.b_2))
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
